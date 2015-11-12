554a31c382d574c60719175fe4eebebde2702f2e
RobertPeek <robert@delta-net.co.uk>
2015-11-12 10:35:26 +0000

    Added more checks for audio tag

--- js/adapt-hotgrid.js
+++ js/adapt-hotgrid.js
@@ -130,7 +130,7 @@ define(function(require) {
             this.evaluateCompletion();
 
             ///// Audio /////
-            if (this.model.get('_audio')) {
+            if (this.model.has('_audio') && this.model.get('_audio')._isEnabled) {
                 // Determine which filetype to play
                 if (Adapt.audio.audioClip[this.model.get('_audio')._channel].canPlayType('audio/ogg')) this.audioFile = currentItem._audio.ogg;
                 if (Adapt.audio.audioClip[this.model.get('_audio')._channel].canPlayType('audio/mpeg')) this.audioFile = currentItem._audio.mp3;
